90dffdad39977e415511e3f7ff5fe96046cb2ef3
vitorSantanaDev <vitorsantana.developer@gmail.com>
2023-04-07 00:18:46 -0300

    chore: :wrench: improving test setup

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.ts(x)?',
+    '!src/**/types.ts',
+    '!src/styles/*.ts'
+  ],
+  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
+  modulePaths: ['<rootDir>/src/', '<rootDir>/.jest'],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }]
+  }
+}
