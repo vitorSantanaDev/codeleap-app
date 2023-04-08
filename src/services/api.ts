28ab70a6e180eb2a44213b0295cf94b24fa274df
vitorSantanaDev <vitorsantana.developer@gmail.com>
2023-04-08 02:02:27 -0300

    chore: change name enviroment variable

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,6 @@
 import axios from 'axios'
 
-// const APP_URL = process.env.REACT_APP_API_URL_LOCAL
-const APP_URL = process.env.REACT_APP_API_URL_PRODUCTION
+const APP_URL = process.env.REACT_APP_API_URL_DEVELOPMENT
 
 const axiosInstance = axios.create({
   baseURL: APP_URL
