9eb7a0bbe76c60ed2fa0ef13861e45686958e0aa
vitorSantanaDev <vitorsantana.developer@gmail.com>
2023-04-08 00:14:14 -0300

    feat: :sparkles: configuring React Query and Toastfy

--- src/index.tsx
+++ src/index.tsx
@@ -10,16 +10,26 @@ import { PersistGate } from 'redux-persist/integration/react'
 import theme from 'styles/theme'
 import { GlobalStyles } from 'styles/global-styles'
 
+import { QueryClientProvider, QueryClient } from 'react-query'
+import { ToastContainer } from 'react-toastify'
+
+import 'react-toastify/dist/ReactToastify.min.css'
+
+const queryClient = new QueryClient()
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
 root.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <ThemeProvider theme={theme}>
-          <GlobalStyles />
-          <App />
-        </ThemeProvider>
+        <QueryClientProvider client={queryClient}>
+          <ThemeProvider theme={theme}>
+            <GlobalStyles />
+            <App />
+            <ToastContainer />
+          </ThemeProvider>
+        </QueryClientProvider>
       </PersistGate>
     </Provider>
   </React.StrictMode>
