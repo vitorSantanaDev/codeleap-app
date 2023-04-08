a9cfb6b13dffc1baa756cb3dedb4d8fc3b9cf646
vitorSantanaDev <vitorsantana.developer@gmail.com>
2023-04-08 00:05:05 -0300

    feat: :art: creating Loading component

---
+++ src/components/Loading/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import * as S from './styles'
+
+const Loading = () => {
+  return (
+    <S.LoadingWrapper>
+      <S.SpinnerContainer />
+    </S.LoadingWrapper>
+  )
+}
+
+export default Loading
---
+++ src/components/Loading/test.spec.tsx
@@ -0,0 +1,48 @@
+import React from 'react'
+
+import { renderWithProviders } from 'utils/render-with-theme'
+import Loading from '.'
+
+describe('<Loading />', () => {
+  it('should render Loading correctly', () => {
+    const { container } = renderWithProviders(<Loading />)
+    expect(container.firstChild).toMatchInlineSnapshot(`
+      .c1 {
+        display: inline-block;
+        width: 50px;
+        height: 50px;
+        border: 3px solid #999999;
+        border-radius: 50%;
+        border-top-color: #7695EC;
+        -webkit-animation: ixZrDe 1s ease-in-out infinite;
+        animation: ixZrDe 1s ease-in-out infinite;
+        -webkit-animation: ixZrDe 1s ease-in-out infinite;
+      }
+
+      .c0 {
+        width: 100%;
+        display: -webkit-box;
+        display: -webkit-flex;
+        display: -ms-flexbox;
+        display: flex;
+        -webkit-box-pack: center;
+        -webkit-justify-content: center;
+        -ms-flex-pack: center;
+        justify-content: center;
+        -webkit-align-items: center;
+        -webkit-box-align: center;
+        -ms-flex-align: center;
+        align-items: center;
+        min-height: 50rem;
+      }
+
+      <div
+        class="c0"
+      >
+        <div
+          class="c1"
+        />
+      </div>
+    `)
+  })
+})
