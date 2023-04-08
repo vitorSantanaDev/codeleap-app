fc4a5227afa58d27fb0406f89d4efd88df887e1c
vitorSantanaDev <vitorsantana.developer@gmail.com>
2023-04-08 00:09:27 -0300

    feat: :sparkles: creating test util with application providers

--- src/utils/render-with-theme.tsx
+++ src/utils/render-with-theme.tsx
@@ -1,8 +1,44 @@
-import React, { ReactElement } from 'react'
+import { Provider } from 'react-redux'
+import React, { PropsWithChildren, ReactElement } from 'react'
 
 import theme from 'styles/theme'
-import { render } from '@testing-library/react'
 import { ThemeProvider } from 'styled-components'
+import { RenderOptions, render } from '@testing-library/react'
+
+import { RootState } from 'redux/store'
+import { userReducer } from 'redux/actions/user-slice'
+import { PreloadedState, configureStore } from '@reduxjs/toolkit'
+import { QueryClient, QueryClientProvider } from 'react-query'
 
 export const renderWithTheme = (component: ReactElement) =>
   render(<ThemeProvider theme={theme}>{component}</ThemeProvider>)
+
+interface ExtendedRenderOptions extends Omit<RenderOptions, 'queries'> {
+  initialState?: PreloadedState<RootState>
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  store?: any
+}
+
+export const renderWithProviders = (
+  ui: React.ReactElement,
+  {
+    initialState = {},
+    store = configureStore({
+      reducer: { user: userReducer },
+      preloadedState: initialState
+    }),
+    ...renderOptions
+  }: ExtendedRenderOptions = {}
+) => {
+  function Wrapper({ children }: PropsWithChildren<unknown>): JSX.Element {
+    return (
+      <QueryClientProvider client={new QueryClient()}>
+        <Provider store={store}>
+          <ThemeProvider theme={theme}>{children}</ThemeProvider>
+        </Provider>
+      </QueryClientProvider>
+    )
+  }
+
+  return { store, ...render(ui, { wrapper: Wrapper, ...renderOptions }) }
+}
